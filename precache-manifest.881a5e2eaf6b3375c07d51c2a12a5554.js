f8abcf000579c59f44a3b9cf782128ad5afb94b0
Ahmed Afifi <ahmed.mohsem.afifi@gmail.com>
2019-07-18 21:40:36 +0400

    Updates

---
+++ precache-manifest.881a5e2eaf6b3375c07d51c2a12a5554.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3c9621d225be9482995825c644e86db",
+    "url": "/mosters-rolodex/index.html"
+  },
+  {
+    "revision": "528a036dfc6a692b4171",
+    "url": "/mosters-rolodex/static/css/main.b9ce1c42.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/mosters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "528a036dfc6a692b4171",
+    "url": "/mosters-rolodex/static/js/main.30dc628f.chunk.js"
+  },
+  {
+    "revision": "253f6367d1a1bea6b3cd",
+    "url": "/mosters-rolodex/static/js/runtime~main.7c72627f.js"
+  }
+]);
